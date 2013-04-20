f722795dcd5c87409d2632a35824a5a955980fff
Matheus Araujo <0matheus.araujo0@gmail.com>
2013-04-20 15:22:37 -0300

    First Prototype

--- tpv_project/views.py
+++ tpv_project/views.py
@@ -9,13 +9,18 @@ from .models import (
     )
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'tpv_project'}
+@view_config(route_name='home', renderer='templates/index.pt')
+def home_view(request):
+    return {}
+    # try:
+    #     one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
+    # except DBAPIError:
+    #     return Response(conn_err_msg, content_type='text/plain', status_int=500)
+    # return {'one': one, 'project': 'tpv_project'}
+
+@view_config(route_name='about', renderer='templates/about.pt')
+def about_view(request):
+    return {}
 
 conn_err_msg = """\
 Pyramid is having a problem using your SQL database.  The problem
